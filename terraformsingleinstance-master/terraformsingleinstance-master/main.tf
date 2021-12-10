9a4c6f2708ce0036a80e7bb2def2e59ceb2cca0d
gallajagadeesh <91022887+gallajagadeesh@users.noreply.github.com>
2021-12-10 11:50:25 +0530

    Update main.tf

--- terraformsingleinstance-master/terraformsingleinstance-master/main.tf
+++ terraformsingleinstance-master/terraformsingleinstance-master/main.tf
@@ -94,16 +94,16 @@ resource "aws_security_group" "allow_all" {
 # data "aws_ami" "my_ami" {
 #      most_recent      = true
 #      #name_regex       = "^mavrick"
-#      owners           = ["721834156908"]
+#      owners           = ["559155668393"]
 # }
 
 
 resource "aws_instance" "web-1" {
-    ami = var.imagename
-    #ami = "ami-0d857ff0f5fc4e03b"
+    #ami = var.imagename
+    ami = "ami-0f6024c31ebd5787b"
     availability_zone = "us-east-1a"
     instance_type = "t2.micro"
-    key_name = "LaptopKey"
+    key_name = "blue.pem"
     subnet_id = "${aws_subnet.subnet1-public.id}"
     vpc_security_group_ids = ["${aws_security_group.allow_all.id}"]
     associate_public_ip_address = true	
